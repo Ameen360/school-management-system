f5d1b14c1fce73d96e41d5d3695958c84e0e9c11
offatechcommuniy <offatechcommunity@gmail.com>
2026-03-02 11:36:01 +0100

    fix(timezone): change timezone from Asia/Kolkata to Africa/Lagos (WAT UTC+1)

--- shared/_footer.php
+++ shared/_footer.php
@@ -1,47 +1,48 @@
 <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4">
-          <!-- Insert website logo -->
-          <img src="images/company-logo.jpg" alt="Website Logo">
-          <!-- Display visitor count -->
-          <p>SCHOOL MANAGEMENT SYSTEM</p>
-          <!-- Display time zone -->
-          <p>Q9P3+75H, My Town, My city, My Country </p>
-        </div>
-        <div class="col-md-4">
-          <p>Time Zone: <?php
-                        date_default_timezone_set('Asia/Kolkata');
-                        $current_time = date('D M d Y H:i:s \G\M\TO (T)');
-                        echo "<p>$current_time</p>";
-                        ?></p>
-        </div>
-        <div class="col-md-4">
-          <div class="footer-links">
-           
-          <p>follow us on</p>
-          </div>
-          <div class="social-icons">
-            <a href="#"><i class="fab fa-facebook-f facebook"></i></a>
-            <a href="#"><i class="fa-brands fa-x-twitter twitter"></i></a>
-            <a href="#"><i class="fab fa-instagram instagram"></i></a>
-            <a href="#"><i class="fab fa-linkedin-in linked-in"></i></a>
-          </div>
-        </div>
+  <div class="container">
+    <div class="row">
+
+      <!-- Column 1: Logo & Address -->
+      <div class="col-md-4">
+        <img src="images/company-logo.jpg" alt="Website Logo">
+        <p>SCHOOL MANAGEMENT SYSTEM</p>
+        <p>Q9P3+75H, My Town, My City, My Country</p>
       </div>
-      <div class="row mt-4">
-        <div class="col-md-12">
-          <p>&copy; <?php echo date('Y'); ?> By <a href="https://www.github.com/ProjectsAndPrograms" target="_blank">ProjectsAndPrograms</a>. All rights reserved.</p>
+
+      <!-- Column 2: Timezone -->
+      <div class="col-md-4">
+        <?php
+        date_default_timezone_set('Africa/Lagos');
+        $current_time = date('D M d Y H:i:s \G\M\TO (T)');
+        ?>
+        <p>Time Zone: <?php echo $current_time; ?></p>
+      </div>
+      <div class="col-md-4">
+        <div class="footer-links">
+
+          <p>follow us on</p>
+        </div>
+        <div class="social-icons">
+          <a href="#"><i class="fab fa-facebook-f facebook"></i></a>
+          <a href="#"><i class="fa-brands fa-x-twitter twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram instagram"></i></a>
+          <a href="#"><i class="fab fa-linkedin-in linked-in"></i></a>
         </div>
       </div>
     </div>
-  </footer>
+    <div class="row mt-4">
+      <div class="col-md-12">
+        <p>&copy; <?php echo date('Y'); ?> By <a href="https://github.com/Ameen360/school-management-system" target="_blank">RayTech</a>. All rights reserved.</p>
+      </div>
+    </div>
+  </div>
+</footer>
 
 
 
-  <script src="https://kit.fontawesome.com/a81368914c.js"></script>
-  <script src="js/bootstrap.bundle.js"></script>
-  <script src="./shared/app.js"></script>
+<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+<script src="js/bootstrap.bundle.js"></script>
+<script src="./shared/app.js"></script>
 </body>
 
 </html>
